eb483b4cd8b71f0073f2452ff9d0a11ca3e6fbf4
Prashant Bansal <prashantbansal500@gmail.com>
2021-06-25 12:11:52 +0530

    script added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container">
           <a class="navbar-brand" href="#">
             Company Name Here
