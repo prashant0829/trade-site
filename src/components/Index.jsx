33c1ade8903cc1ffc49f7ac56c0d94d219ee40da
Prashant Bansal <prashantbansal500@gmail.com>
2021-06-27 09:56:19 +0530

    parallax added

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -7,10 +7,17 @@ const index = () => {
       <div className="landing">
         <div class="lefty"></div>
         <div class="righty">
-          <img src="img/42a.jpg" style={{width:" 450px", height:"650px" }}></img>
+          <img
+            src="img/42a.jpg"
+            style={{ width: " 450px", height: "650px" }}></img>
         </div>
       </div>
-      
+
+      <div
+        class="parallax"
+        style={{ backgroundImage: `url("/img/parallax.jpg")` }}>
+        <div className="parallax-text">Hello There I am</div>
+      </div>
     </div>
   );
 };
