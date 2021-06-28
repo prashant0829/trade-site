f6234e102a0b50d1662f62c8355e348228c72000
prachi chauhan <prachichauhan0101@gmail.com>
2021-06-28 10:28:28 +0530

    newone

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -8,14 +8,15 @@ const index = () => {
         <div
         class="index"
         style={{ backgroundImage: `url("/img/32.jpg")` }}>
-        <div className="index-bar"><h3 class="text">We Focus on Building Relationships & Providing Best Quality to Our Buyers.</h3></div>
+        <div className="index-bar"><h2
+         class="texts">We Focus on Building Relationships & Providing Best Quality to Our Buyers.</h2></div>
       </div>
      
 
       <div
         class="parallax"
         style={{ backgroundImage: `url("/img/parallax.jpg")` }}>
-        <div className="parallax-text">Welcome to Primex india</div>
+        <div className="parallax-text">Welcome to Primex India...</div>
       </div>
     </div>
   );
