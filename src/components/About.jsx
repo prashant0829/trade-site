f2043bf36837569a45a36afa095dbf4bbd280c17
prachi chauhan <prachichauhan0101@gmail.com>
2021-06-29 22:03:20 +0530

    chngess

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,29 +3,49 @@ import React from "react";
 const about = () => {
   return (
     <>
+     <section id="bg-color">
+    
       <div className="container">
         <div className="text-center"><br/><br/><br/>
-          <p className="secondary-heading">This Is Us</p>
+          <p class="secondary-heading">This Is Us</p>
           <p className="primary-heading">Since 1992</p>
-        </div><br/>
+        </div>
         <div class="row">
-          <div class="col-md-4">
-            <h3 class="about-h3 text-uppercase"> Who we are</h3>
+          <div class="col-md-6">
             <div class="box-padding-10">
-              <p class="text-muted about-p"><br/>
+              <h3 class="about-h3">WHO WE ARE</h3><br/>
+              <p class="text-muted">
                 We are a quality driven artisan & exporters of premium
                 metal/wooden home-decor, garden, kitchen-ware items and other
                 indian handicraft items based from Moradabad City. Our products
                 are designed oriented and made especially for those who believe
-                in having the best of everything.At Primex India,the skilled craftsmen
-              create handcrafted products to the highest level.
-               We are a highly regarded
-               Import and Export Consultancy dedicated to providing 
-               various types of products and services. We are specialised for Electro
-                Phoretic Lacquer (E.P.L) items. We have a complete in-house
+                in having the best of everything. We are specialized for Electro 
+                Phoretic Lacquer (E.P.L) items.<br/><br/> 
+                We have a complete in-house
                 compliance manufacturing, polishing, plating and other finishes
                 set up in our factory. We are into this handicraft industry
-                since 1992.<br/>To give our customers utmost satisfaction, we
+                since 1992.
+                
+                 We ensure accurate communication when
+                  planning orders together with our 
+                 customers and suppliers, with the
+                 aim of securing a consistent cooperation.<br/>
+                 <br/>
+
+                "At Primex India Inc." we never compromised in
+                our products quality because our customer's 
+                satisfaction is the biggest thing for us because
+                we believe in good & long-lasting business
+                 relationship with any of our customers. </p>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <h3 class="about-h3">WHAT WE DO</h3>
+            <div class="box-padding-10">
+              
+              <p class="text-muted"><br/>
+               To give our customers utmost satisfaction, we
               provide customization facility to all our clients. We are specialized in the 
                   most exclusive high-end products.We provide efficient 
                 services due to our vast network, 
@@ -33,56 +53,28 @@ const about = () => {
                  with the highest quality possible. We incorporate a customer-focused approach,
                   ensuring effective communication and customer loyalty, while 
                   applying a service program that serves the needs 
-                  of our buyers. <br/>We have marched at the forefront 
+                  of our buyers.
+              </p>
+               <p class="text-muted">
+                We have marched at the forefront 
                  of the handicraft industry as a manufacturer and exporter of 
                  superior quality handcrafted items like candle holders,Accent Furniture,
                   wall decorative, Serving Trays, Vases, Wire Baskets,Planters, Globes,
                   Kitchen Ware Items, Photo Frames, Objects & Sculptures, Table-Tops,
                   Hooks/Wall Items, Mirrors, Christmas Items, Iron Wire Baskets and more such products.
-                  We ensure accurate communication when planning orders 
-               together with our customers and suppliers, with the 
-               aim of securing a consistent cooperation.. 
+                  Our customers are guaranteed to have access to the highest quality of products, 
+                  designed to meet customer demands, from the most discerning markets.
               </p>
+
+             
+             
             </div>
           </div>
-          <div class="col-md-4">
-            <h3 class="about-h3">OUR HISTORY</h3>
-              <div class="box-padding-10">
-                  
-                </div>
-              </div>
-          </div>
-                  
-          <div class="col-md-4">
-            <h3 class="about-h3">Our Collection Includes </h3>
-              <div class="box-padding-10">
-              <div class="box-padding-left">
-                <p class="text-muted"><br/>
-                  1. Candle-Holders
-                  <br />
-                  2. Globes
-                  <br />
-                  3. Kitchen Ware Items
-                  <br />
-                  4. Photo Frames
-                  <br />
-                  5. Objects & Sculptures
-                  <br />
-                  6. Table-Tops
-                  <br />
-                  7. Hooks/Wall Items
-                  <br />
-                  8. Mirrors
-                  <br />
-                  9. Christmas Items
-                  <br />
-                  10. Iron Wire Baskets
-                </p> 
-              </div>
-              </div>
-            </div>
+
+          
         </div>
-      <div/>
+      </div>
+    </section>
     </>
   );
 };
--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -18,34 +18,66 @@ const index = () => {
       </div>
       <br />
       <div className="container product">
-        <h1>OU</h1>
+        <h1 class=" primary-heading text-center">OUR PRODUCTS</h1>
         <div class="row">
-          <div class="col-lg-4 col-md-4 col-sm-4 col-sx-12  p-2">
+          <div class="col-lg-4 col-md-4 col-sm-4 col-sx-12 p-2">
             <div
               className="rounded"
               style={{
                 backgroundImage: `url("/img/wood.jpg")`,
-                minHeight: "500px",
+                minHeight: "300px",
                 backgroundSize: "cover",
-              }}></div>
+              }}>
+              <div className="product-text-w"><h4><b>WOODEN ITEMS</b></h4>
+              <ul>
+
+                <li>Kitchen-Ware Items</li>
+                <li>Accent Furniture</li>
+                <li>Photo Frames</li>
+                <li>Wall Arts</li>
+                
+               
+              </ul></div>
+              </div>
           </div>
           <div class="col-lg-4 col-md-4 col-sm-4 col-sx-12 p-2">
             <div
               className="rounded"
               style={{
                 backgroundImage: `url("/img/copper.jpg")`,
-                minHeight: "500px",
+                minHeight: "300px",
                 backgroundSize: "cover",
-              }}></div>
+              }}>
+              <div className="product-text-c"><h4><b>COPPER ITEMS</b></h4>
+              <ul >
+                
+                <li>Candle - Holders</li>
+                <li>Wire Baskets</li>
+               
+                <li>Mirrors</li>
+                 <li>Globes</li>
+               
+              </ul></div>
+            </div>
           </div>
           <div class="col-lg-4 col-md-4 col-sm-4 col-sx-12 p-2">
             <div
               className="rounded"
               style={{
                 backgroundImage: `url("/img/brass.jpg")`,
-                minHeight: "500px",
+                minHeight: "300px",
                 backgroundSize: "cover",
-              }}></div>
+              }}>
+              <div className="product-text-cp"><h4><b>BRASS ITEMS</b></h4>
+              <ul >
+                <li>Objects & Sculptures</li>
+                <li>Christmas Items</li>
+                
+                <li>Planters</li>
+                <li>Vases</li>
+                
+              </ul></div>
+              </div>
           </div>
           
         </div>
