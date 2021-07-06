f586034a880d305d4eb844e5721e4be85a446d1e
Prashant Bansal <prashantbansal500@gmail.com>
2021-07-06 16:27:56 +0530

    margin added in collection

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -2,46 +2,100 @@ import React from "react";
 
 const collection = () => {
   return (
-    <div className="text-center"><br/><br/><br/>
+    <div className="text-center mb-5">
+      <br />
+      <br />
+      <br />
       <p class="secondary-heading">Glimpse Of Our Works</p>
       <p className="primary-heading">Our Gallery</p>
       <div className="container">
         <div className="card-columns">
-          <div className="card"><a href="/img/18a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/18a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a
+              href="/img/18a.jpg"
+              className="fancybox"
+              data-fancybox="gallery1">
+              <img src="/img/18a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/17a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/17a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a
+              href="/img/17a.jpg"
+              className="fancybox"
+              data-fancybox="gallery1">
+              <img src="/img/17a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/26a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/26a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a
+              href="/img/26a.jpg"
+              className="fancybox"
+              data-fancybox="gallery1">
+              <img src="/img/26a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/4a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/4a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a href="/img/4a.jpg" className="fancybox" data-fancybox="gallery1">
+              <img src="/img/4a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/5a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/5a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a href="/img/5a.jpg" className="fancybox" data-fancybox="gallery1">
+              <img src="/img/5a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/28a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/28a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a
+              href="/img/28a.jpg"
+              className="fancybox"
+              data-fancybox="gallery1">
+              <img src="/img/28a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/31a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/31a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a
+              href="/img/31a.jpg"
+              className="fancybox"
+              data-fancybox="gallery1">
+              <img src="/img/31a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/9a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/9a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a href="/img/9a.jpg" className="fancybox" data-fancybox="gallery1">
+              <img src="/img/9a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/16a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/16a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a
+              href="/img/16a.jpg"
+              className="fancybox"
+              data-fancybox="gallery1">
+              <img src="/img/16a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/27a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/27a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a
+              href="/img/27a.jpg"
+              className="fancybox"
+              data-fancybox="gallery1">
+              <img src="/img/27a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/29a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/29a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a
+              href="/img/29a.jpg"
+              className="fancybox"
+              data-fancybox="gallery1">
+              <img src="/img/29a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
-          <div className="card"><a href="/img/20a.jpg" className="fancybox" data-fancybox="gallery1">
-            <img src="/img/20a.jpg" className="card-img-top" alt="" /></a>
+          <div className="card">
+            <a
+              href="/img/20a.jpg"
+              className="fancybox"
+              data-fancybox="gallery1">
+              <img src="/img/20a.jpg" className="card-img-top" alt="" />
+            </a>
           </div>
         </div>
       </div>
@@ -50,5 +104,3 @@ const collection = () => {
 };
 
 export default collection;
-
-
